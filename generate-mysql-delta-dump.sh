40b495c28b6a00e05a3ca6004ffa7b93f97bde23
sackot007 <sackot007@users.noreply.github.com>
2016-12-28 19:56:58 -0600

    generate-mysql-delta-dump.sh

---
+++ generate-mysql-delta-dump.sh
@@ -0,0 +1,23 @@
+
+#!/bin/sh
+
+SCHEMA_NAME=$1
+TABLE_NAME=$2
+echo "Generating Delta feed for " $TABLE_NAME " in schema " $SCHEMA_NAME
+FILE=./deltafeed/$SCHEMA_NAME_$TABLE_NAME-delta_`date +"%Y-%m-%d_%T"`.sql
+
+DELTA_CONDITION=''
+
+if [ ! $TABLE_NAME = "Notification" ]
+then
+        echo "In If"
+        DELTA_CONDITION='--where=LastUpdatedTime>CURDATE()-2'
+fi
+
+echo $DELTA_CONDITION
+echo "Run mysqldump command for "$SCHEMA_NAME"."$TABLE_NAME"  delta"
+/usr/bin/mysqldump --defaults-extra-file=/home/adminkmp/dbscripts_delta/dbconfig.prop --replace --no-create-info --no-create-db $SCHEMA_NAME $TABLE_NAME $DELTA_CONDITION >> $FILE
+
+echo "Gzip generated file: "$FILE
+gzip $FILE
+echo "Gzip process of file " $FILE "completed"
